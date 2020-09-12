1585f3c042d0d705055e4813bfebf71b03ff2faa
Nir Geller <gnir.work@gmail.com>
2020-09-12 19:59:59 +0300

    Create basic quiz game.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
+import {PageHeader, Button, Layout, Input, Card, message} from 'antd';
 import './App.css';
+import MyMapComponent from "./Map";
+import {useState} from 'react';
+
+import {Tabs} from 'antd';
+
+const {TabPane} = Tabs;
+
+const QUESTIONS = [
+    {
+        question: 'איפה נולדת?',
+        answer: 'ישראל',
+        location: {lat: -34.397, lng: 150.644},
+        locationName: 'אמריקה'
+
+    }, {
+        question: 'מה השם שלך?',
+        answer: 'ניר',
+        location: {lat: 32.0668273, lng: 34.7630315},
+        locationName: 'תל אביב'
+
+    },
+    {
+        question: 'בן כמה אתה?',
+        answer: '22',
+        location: {lat: 31.7133867, lng: 35.092611517},
+        locationName: 'צור הדסה'
+
+    }
+]
+
+const TREASURE_LOCATION = {lat: 41.7133867, lng: 55.092611517};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [currentAnswer, setCurrentAnswer] = useState("");
+
+    const onAnswerSubmit = () => {
+        if (currentAnswer === QUESTIONS[currentQuestion].answer) {
+            message.success('תשובה נכונה!', 1);
+            setTimeout(moveOnToNextQuestion, 1000);
+        } else {
+            message.error('טעות!', 1);
+        }
+
+    }
+
+    const moveOnToNextQuestion = () => {
+        setCurrentQuestion(currentQuestion + 1);
+        setCurrentAnswer("");
+    }
+
+    const onAnswerChange = (newAnswer) => {
+        setCurrentAnswer(newAnswer.target.value);
+    }
+
+    const getQuestionTitle = (index) => (index <= currentQuestion ?
+        `שאלה  ${index + 1} - ${QUESTIONS[index].locationName}` :
+        `שאלה  ${index + 1} - ???`)
+
+    return (
+        <Layout stlye={{direction: "ltr"}}>
+            <PageHeader
+                className="site-page-header"
+                onBack={() => null}
+                title="מצא את המטמון"
+                subTitle="בהצלחה בחיפוש הכי קשה בעולם"
+            />
+            <div className="App">
+
+                {currentQuestion < QUESTIONS.length ? (
+                    <>
+                        <MyMapComponent isMarkerShown center={QUESTIONS[currentQuestion].location}/>
+
+                        <Tabs activeKey={currentQuestion.toString()}>
+                            {
+                                QUESTIONS.map(({question}, index) => (
+                                    <TabPane tab={getQuestionTitle(index)} key={index}>
+                                        <Card style={{width: "40vw", margin: 'auto'}}>
+                                            <h3> {question} </h3>
+                                            <Input placeholder="תכניס את התשובה שלך פה..." onChange={onAnswerChange}
+                                                   value={currentAnswer}/>
+                                            <Button style={{marginTop: '1em'}} onClick={onAnswerSubmit}>
+                                                הגש תשובה
+                                            </Button>
+                                        </Card>
+                                    </TabPane>
+                                ))
+                            }
+                        </Tabs>
+                    </>
+                ) : (
+                    <>
+                        <h1> מצאת את האוצר! </h1>
+                        <MyMapComponent treasure isMarkerShown center={TREASURE_LOCATION}/>
+                    </>
+                )}
+            </div>
+        </Layout>
+    );
 }
 
 export default App;
---
+++ src/Map.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {compose, withProps} from "recompose";
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+} from "react-google-maps";
+import Treasure from './treasure.png'
+
+const MyMapComponent = compose(
+    withProps({
+        /**
+         * Note: create and replace your own key in the Google console.
+         * https://console.developers.google.com/apis/dashboard
+         * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
+         */
+        googleMapURL:
+            "https://maps.googleapis.com/maps/api/js?key=AIzaSyDj42tuYtW8WuCz9fNMCCcTlrT1t1bL0O4&v=3.exp&libraries=geometry,drawing,places&language=he",
+        loadingElement: <div style={{height: `100%`}}/>,
+        containerElement: <div style={{height: `400px`}}/>,
+        mapElement: <div style={{height: `100%`}}/>
+    }),
+    withScriptjs,
+    withGoogleMap
+)(props => (
+    <GoogleMap l defaultZoom={8} center={props.center}>
+        {props.isMarkerShown && (
+            <Marker position={props.center} icon={{
+                url: props.treasure ? Treasure : null
+            }}/>
+        )}
+    </GoogleMap>
+));
+
+export default MyMapComponent;
