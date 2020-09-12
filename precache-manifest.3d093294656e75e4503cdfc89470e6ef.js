7b82bc038b913ae946a018492416ad3a9b1bf1ed
Nir Geller <gnir.work@gmail.com>
2020-09-12 20:04:03 +0300

    Updates

---
+++ precache-manifest.3d093294656e75e4503cdfc89470e6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d47c1ca3a5ea6efaa588c5c45a24039",
+    "url": "/oshrita-quiz/index.html"
+  },
+  {
+    "revision": "b236df3c98749196bacb",
+    "url": "/oshrita-quiz/static/css/2.ea06eff6.chunk.css"
+  },
+  {
+    "revision": "433d0d26a61414d95f0d",
+    "url": "/oshrita-quiz/static/css/main.93d9ae81.chunk.css"
+  },
+  {
+    "revision": "b236df3c98749196bacb",
+    "url": "/oshrita-quiz/static/js/2.3114df7c.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/oshrita-quiz/static/js/2.3114df7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433d0d26a61414d95f0d",
+    "url": "/oshrita-quiz/static/js/main.95b2b981.chunk.js"
+  },
+  {
+    "revision": "216cb6ef8036fb0202c9",
+    "url": "/oshrita-quiz/static/js/runtime-main.f70e924a.js"
+  }
+]);
